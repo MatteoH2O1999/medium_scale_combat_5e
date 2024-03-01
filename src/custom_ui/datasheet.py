ebb15ba546217e8b79a05892ecb5faed50d75b79
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2024-03-01 17:37:57 +0100

    Add standard datasheet plot

--- src/custom_ui/datasheet.py
+++ src/custom_ui/datasheet.py
@@ -1,4 +1,5 @@
 import pathlib
+import math
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,9 +7,9 @@ import numpy as np
 from PIL import Image
 from matplotlib.axes import Axes
 from matplotlib import font_manager
-from typing import Tuple
+from typing import Tuple, List
 
-from lib.interfaces import UnitStatBlock, StatBlock
+from lib.interfaces import UnitStatBlock, StatBlock, CreatureAttack
 
 mpl.use("QtAgg")
 resource_folder = pathlib.Path(__file__).parent.parent.joinpath("resources")
@@ -33,25 +34,54 @@ for font in font_files:
 
 LEFT_MARGIN = 100
 RED = "#58170d"
+SPACE_BETWEEN_ATTACKS = 100
 
 
 def draw_separator_line(
-    start: Tuple[float, float], length: float, width: float, ax: Axes
+    start: Tuple[float, float],
+    length: float,
+    width: float,
+    ax: Axes,
+    color=RED,
+    line=False,
 ) -> None:
-    points = [
-        (start[0], start[1] + width / 2),
-        (start[0], start[1] - width / 2),
-        (start[0] + length, start[1]),
-    ]
+    if line:
+        points = [
+            (start[0], start[1] + width / 2),
+            (start[0], start[1] - width / 2),
+            (start[0] + length, start[1] - width / 2),
+            (start[0] + length, start[1] + width / 2),
+        ]
+    else:
+        points = [
+            (start[0], start[1] + width / 2),
+            (start[0], start[1] - width / 2),
+            (start[0] + length, start[1]),
+        ]
     x = [p[0] for p in points]
     y = [p[1] for p in points]
-    ax.fill(x, y, RED)
+    ax.fill(x, y, color)
 
 
 def explicit_sign_str(number: float) -> str:
     if number < 0:
         return str(number)
     return f"+{str(number)}"
+
+
+def multiattack_string(creature_name: str, attack_list: List[CreatureAttack]) -> str:
+    ret = f"The {creature_name} makes "
+    for index, attack in list(enumerate(attack_list, start=0)):
+        remaining_items = len(attack_list) - 1 - index
+        if index > 0:
+            if remaining_items > 0:
+                ret += ", "
+            else:
+                ret += " and "
+        ret += f"{attack.multiattack} {attack.name} attack"
+        if attack.multiattack > 1:
+            ret += "s"
+    return f"{ret}."
 
 
 def datasheet_from_unit_stat_block(stat_block: UnitStatBlock) -> plt.Figure:
@@ -268,6 +298,134 @@ def datasheet_from_stat_block(stat_block: StatBlock) -> plt.Figure:
     )
 
     draw_separator_line((LEFT_MARGIN, 750), 1800, 20, ax)
+
+    # Attack title
+    vertical_position = 875
+
+    attacks_text = ax.text(
+        LEFT_MARGIN,
+        vertical_position,
+        "A",
+        fontfamily="Scala Sans",
+        fontweight="regular",
+        fontsize=28,
+    )
+    ax.annotate(
+        "CTIONS",
+        xycoords=attacks_text,
+        xy=(1, 0),
+        verticalalignment="bottom",
+        fontfamily="Scala Sans",
+        fontweight="regular",
+        fontsize=24,
+    )
+
+    draw_separator_line((LEFT_MARGIN, 900), 1800, 2, ax, "black", True)
+
+    # Multiattacks
+    vertical_position = 975
+
+    multiattack_names = list(stat_block.multiattacks.keys())
+    multiattack_names.sort()
+    for multiattack_name in multiattack_names:
+        multiattack = stat_block.multiattacks[multiattack_name]
+        name_text = ax.text(
+            LEFT_MARGIN,
+            vertical_position,
+            f"{multiattack_name.capitalize()}.",
+            fontfamily="Scala Sans",
+            fontweight="bold",
+            fontstyle="italic",
+            fontsize=17,
+        )
+        ax.annotate(
+            multiattack_string(
+                " ".join(
+                    [part.capitalize() for part in stat_block.name.lower().split(" ")]
+                ),
+                multiattack,
+            ),
+            xycoords=name_text,
+            xy=(1, 0),
+            verticalalignment="bottom",
+            fontfamily="Scala Sans",
+            fontweight="regular",
+            fontsize=17,
+        )
+        vertical_position += SPACE_BETWEEN_ATTACKS
+
+    # Attacks
+    attack_names = list(stat_block.attacks.keys())
+    attack_names.sort()
+    for attack_name in attack_names:
+        attack = stat_block.attacks[attack_name]
+        name_text = ax.text(
+            LEFT_MARGIN,
+            vertical_position,
+            f"{attack.name.capitalize()}. ",
+            fontfamily="Scala Sans",
+            fontweight="bold",
+            fontstyle="italic",
+            fontsize=17,
+        )
+        weapon_text = ax.annotate(
+            f"{'Melee' if attack.is_melee else 'Ranged'} Weapon Attack: ",
+            xycoords=name_text,
+            xy=(1, 0),
+            verticalalignment="bottom",
+            fontfamily="Scala Sans",
+            fontweight="regular",
+            fontstyle="italic",
+            fontsize=17,
+        )
+        to_hit_text = ax.annotate(
+            f"{explicit_sign_str(attack.to_hit_bonus)} to hit, ",
+            xycoords=weapon_text,
+            xy=(1, 0),
+            verticalalignment="bottom",
+            fontfamily="Scala Sans",
+            fontweight="regular",
+            fontsize=17,
+        )
+        range_text = ax.annotate(
+            f"{'reach' if attack.is_melee else 'range'} {attack.range} ft., ",
+            xycoords=to_hit_text,
+            xy=(1, 0),
+            verticalalignment="bottom",
+            fontfamily="Scala Sans",
+            fontweight="regular",
+            fontsize=17,
+        )
+        target_text = ax.annotate(
+            f"{attack.target.description}. ",
+            xycoords=range_text,
+            xy=(1, 0),
+            verticalalignment="bottom",
+            fontfamily="Scala Sans",
+            fontweight="regular",
+            fontsize=17,
+        )
+        hit_italic_text = ax.annotate(
+            "Hit: ",
+            xycoords=target_text,
+            xy=(1, 0),
+            verticalalignment="bottom",
+            fontfamily="Scala Sans",
+            fontweight="regular",
+            fontstyle="italic",
+            fontsize=17,
+        )
+        ax.annotate(
+            f"{math.floor(attack.total_average_damage)} damage.",
+            xycoords=hit_italic_text,
+            xy=(1, 0),
+            verticalalignment="bottom",
+            fontfamily="Scala Sans",
+            fontweight="regular",
+            fontsize=17,
+        )
+
+        vertical_position += SPACE_BETWEEN_ATTACKS
 
     return figure
 
