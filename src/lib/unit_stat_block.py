28a8a8c7cc36871febdc47daebc8657763b9356c
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2024-03-24 18:35:46 +0100

    Add invulnerable saving throws

--- src/lib/unit_stat_block.py
+++ src/lib/unit_stat_block.py
@@ -1,5 +1,5 @@
 import math
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 from .interfaces import (
     UnitStatBlock as UnitStatBlockInterface,
@@ -21,6 +21,7 @@ class UnitStatBlock(UnitStatBlockInterface):
         speed: int,
         resistance: int,
         saving_throw: int,
+        invulnerable_saving_throw: Optional[int],
         hp: int,
         attacks: List[UnitAttack],
         multiattacks: Dict[str, List[UnitAttack]],
@@ -31,6 +32,7 @@ class UnitStatBlock(UnitStatBlockInterface):
         :param speed: The unit's speed
         :param resistance: The unit's resistance value
         :param saving_throw: The unit's armor saving throw
+        :param invulnerable_saving_throw: The unit's invulnerable armor saving throw
         :param hp: The hit point of each creature in the unit
         :param attacks: The attacks this unit may perform
         :param multiattacks: The multiattacks this unit may perform
@@ -50,6 +52,14 @@ class UnitStatBlock(UnitStatBlockInterface):
         if not isinstance(saving_throw, int) or saving_throw < 2 or saving_throw > 6:
             raise InvalidStatBlockParamError(
                 f"saving_throw value should be an integer between 2 and 6. Got {saving_throw}."
+            )
+        if invulnerable_saving_throw is not None and (
+            not isinstance(invulnerable_saving_throw, int)
+            or invulnerable_saving_throw < 2
+            or invulnerable_saving_throw > 6
+        ):
+            raise InvalidStatBlockParamError(
+                f"invulnerable_saving_throw should be an integer between 2 and 6 or None. Got {invulnerable_saving_throw}"
             )
         if not isinstance(hp, int) or hp < 1:
             raise InvalidStatBlockParamError(
@@ -82,6 +92,7 @@ class UnitStatBlock(UnitStatBlockInterface):
         self._speed: int = speed
         self._resistance: int = resistance
         self._saving_throw: int = saving_throw
+        self._invulnerable_saving_throw: Optional[int] = invulnerable_saving_throw
         self._hp: int = hp
         self._attacks: List[UnitAttack] = attacks
         self._multiattacks: Dict[str, List[UnitAttack]] = multiattacks
@@ -97,6 +108,10 @@ class UnitStatBlock(UnitStatBlockInterface):
     @property
     def saving_throw(self) -> int:
         return self._saving_throw
+
+    @property
+    def invulnerable_saving_throw(self) -> Optional[int]:
+        return self._invulnerable_saving_throw
 
     @property
     def hit_points(self) -> int:
@@ -136,7 +151,26 @@ def _saving_throw_value_from_stat_block(stat_block: StatBlock) -> int:
     return 2
 
 
-def _hit_points_per_creature_from_stat_block(stat_block: StatBlock) -> int:
+def _invulnerable_saving_throw_value_from_stat_block(
+    stat_block: StatBlock,
+) -> Optional[int]:  # pragma: no cover
+    ac = stat_block.armor_class
+    if ac < 18:
+        return None
+    elif ac < 20:
+        return 6
+    elif ac < 22:
+        return 5
+    elif ac < 25:
+        return 4
+    elif ac < 30:
+        return 3
+    return 2
+
+
+def _hit_points_per_creature_from_stat_block(
+    stat_block: StatBlock,
+) -> int:  # pragma: no cover
     return math.ceil(stat_block.hit_points / 20)
 
 
@@ -144,6 +178,9 @@ def from_stat_block(stat_block: StatBlock) -> UnitStatBlock:
     speed = _speed_from_stat_block(stat_block)
     resistance = _resistance_value_from_stat_block(stat_block)
     saving_throw = _saving_throw_value_from_stat_block(stat_block)
+    invulnerable_saving_throw = _invulnerable_saving_throw_value_from_stat_block(
+        stat_block
+    )
     hp = _hit_points_per_creature_from_stat_block(stat_block)
     attacks = []
     for attack in stat_block.attacks.values():
@@ -155,5 +192,12 @@ def from_stat_block(stat_block: StatBlock) -> UnitStatBlock:
             multi.append(from_creature_attack(attack))
         multiattacks[multiattack_name] = multi
     return UnitStatBlock(
-        stat_block.name, speed, resistance, saving_throw, hp, attacks, multiattacks
+        stat_block.name,
+        speed,
+        resistance,
+        saving_throw,
+        invulnerable_saving_throw,
+        hp,
+        attacks,
+        multiattacks,
     )
